7984fdcb10e34bf5ad980e976d4b00a118cb5741
Andrey Nikolaev <nikolaevandronik@yandex.ru>
2024-01-23 18:04:39 +0300

    заменил date(now) на current_date

--- backend/src/main/resources/db/migration/V002__insert_data.sql
+++ backend/src/main/resources/db/migration/V002__insert_data.sql
@@ -4,5 +4,5 @@ INSERT INTO product (id, name, picture_url, price) VALUES (3, 'Молочная'
 INSERT INTO product (id, name, picture_url, price) VALUES (4, 'Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315.00);
 INSERT INTO product (id, name, picture_url, price) VALUES (5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00);
 INSERT INTO product (id, name, picture_url, price) VALUES (6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
-INSERT INTO orders (id, status, date_created) SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW()+'90 days' - NOW()))) FROM generate_series(1, 10000000) s(i);
+INSERT INTO orders (id, status, date_created) SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)],(CURRENT_DATE - (random() * (CURRENT_DATE+'90 days' - CURRENT_DATE))) FROM generate_series(1, 10000000) s(i);
 INSERT INTO order_product (quantity, order_id, product_id) SELECT floor(1+random()*50)::int, i, 1 + floor(random()*6)::int % 6 FROM generate_series(1, 10000000) s(i);
