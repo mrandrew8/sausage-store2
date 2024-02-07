b571cdc76937730df9705ba76485962e8e747e94
Andrey Nikolaev <nikolaevandronik@yandex.ru>
2024-02-07 19:13:45 +0300

    отключи фронтенд, поправил стейджы для бэкенда и обновил deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,16 +1,28 @@
 #! /bin/bash
 #Если свалится одна из команд, рухнет и весь скрипт
 set -xe
+sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
+sudo docker network create -d bridge sausage_network || true
+sudo docker rm -f sausage-backend || true
+sudo docker run -d --name sausage-backend \ # пока что убрал --rm, чтоб мониторить работу контейнера
+     --env SPRING_DATASOURCE_URL="${SPRING_DATASOURCE_URL}" \
+     --env SPRING_DATASOURCE_USERNAME="${SPRING_DATASOURCE_USERNAME}" \
+     --env SPRING_DATASOURCE_PASSWORD="${SPRING_DATASOURCE_PASSWORD}" \
+     --env SPRING_DATA_MONGODB_URI="${SPRING_DATA_MONGODB_URI}" \
+     --network=sausage_network \
+     "${CI_REGISTRY_IMAGE}"/sausage-backend:latest 
+
+# Старый код
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
-sudo cp -rf backend.service /etc/systemd/system/backend.service #тут мы перекидывай из /home/student файлик backend.service в /etc/systemd/system
-sudo rm -f /opt/sausage-store/bin/sausage-store*.jar||true #далее удаляем старый джарник
-#Переносим артефакт в нужную папку
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-#здесь мы скачаваем джарник sausage-store-${VERSION}.jar с нексуса и сохраняем его по имени sausage-store.jar. Для скачивания используем креды, которые
-#зашили в переменные в гитлабе ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS}
-sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай, перекидываем 
-# файлик в  директорию, где изначально хранился наш джарник
-#Обновляем конфиг systemd с помощью рестарта
-sudo systemctl daemon-reload
-#Перезапускаем сервис сосисочной
-sudo systemctl restart backend 
+# sudo cp -rf backend.service /etc/systemd/system/backend.service #тут мы перекидывай из /home/student файлик backend.service в /etc/systemd/system
+# sudo rm -f /opt/sausage-store/bin/sausage-store*.jar||true #далее удаляем старый джарник
+# #Переносим артефакт в нужную папку
+# curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+# #здесь мы скачаваем джарник sausage-store-${VERSION}.jar с нексуса и сохраняем его по имени sausage-store.jar. Для скачивания используем креды, которые
+# #зашили в переменные в гитлабе ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS}
+# sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай, перекидываем 
+# # файлик в  директорию, где изначально хранился наш джарник
+# #Обновляем конфиг systemd с помощью рестарта
+# sudo systemctl daemon-reload
+# #Перезапускаем сервис сосисочной
+# sudo systemctl restart backend 
