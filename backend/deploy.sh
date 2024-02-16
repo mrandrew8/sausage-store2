a97545a9580db7177b196457de9ecb2928877548
Andrey Nikolaev <nikolaevandronik@yandex.ru>
2024-02-16 23:01:17 +0300

    поправил дирекотрию для докер композа

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,7 +3,7 @@
 set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge sausage_network || true
-cd /home/student/sausage-backend/docker-compose.yml|| true
+cd /home/student/sausage-backend|| true
 sudo docker-compose rm -s -f backend || true
 sudo docker-compose up -d sausage-backend
 
