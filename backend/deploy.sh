68eaf3c51c76a21fe7037e656abadfb77e4c1130
Andrey Nikolaev <nikolaevandronik@yandex.ru>
2024-02-24 18:14:11 +0300

    поправил переменные в деплой сш

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -12,6 +12,8 @@ export BACKEND_VERSION=${BACKEND_VERSION}
 export BACKEND_REPORT_VERSION=${BACKEND_REPORT_VERSION}
 export REPORTS_MONGODB_URI=${REPORTS_MONGODB_URI}
 export FRONTEND_VERSION=${FRONTEND_VERSION}
+export BLUE_SERVICE="backendblue"
+export GREEN_SERVICE="backendgreen"
 docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 echo ${BACKEND_VERSION}
 docker network create -d bridge sausage_network || true
@@ -19,24 +21,17 @@ echo ${REPORTS_MONGODB_URI}
 pwd
 cd /home/student/sausage-store2|| true
 
-if docker ps --format "{{.Names}}" | grep "backendblue"; then
-  export ACTIVE_SERVICE=backendblue
-  export INACTIVE_SERVICE=backendgreen
-  docker-compose pull $INACTIVE_SERVICE
-  docker-compose up -d $INACTIVE_SERVICE
-  docker-compose rm -s -f $ACTIVE_SERVICE
-elif docker ps --format "{{.Names}}" | grep "backendgreen"; then
-  export ACTIVE_SERVICE=backendgreen
-  export INACTIVE_SERVICE=backendblue
-  docker-compose pull $INACTIVE_SERVICE
-  docker-compose up -d $INACTIVE_SERVICE
-  docker-compose rm -s -f $ACTIVE_SERVICE
+if docker ps --format "{{.Names}}" | grep "$BLUE_SERVICE"; then
+  docker-compose pull $GREEN_SERVICE
+  docker-compose up -d $GREEN_SERVICE
+  docker-compose rm -s -f $BLUE_SERVICE
+elif docker ps --format "{{.Names}}" | grep "$GREEN_SERVICE"; then
+  docker-compose pull $BLUE_SERVICE
+  docker-compose up -d $BLUE_SERVICE
+  docker-compose rm -s -f $GREEN_SERVICE
 else
-  export ACTIVE_SERVICE=""
-  export INACTIVE_SERVICE=backendblue
-  docker-compose pull $INACTIVE_SERVICE
-  docker-compose up -d $INACTIVE_SERVICE
-  docker-compose rm -s -f $ACTIVE_SERVICE
+  docker-compose pull $BLUE_SERVICE
+  docker-compose up -d $BLUE_SERVICE
 fi
 
 # docker-compose rm -s -f backend || true
