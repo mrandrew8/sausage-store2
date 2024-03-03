d944d640c773ce7af1fd4d31db6fb5f82c7a1785
Andrey Nikolaev <nikolaevandronik@yandex.ru>
2024-03-03 20:41:22 +0300

    посмотрим что сохраняется в переменные

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -16,6 +16,9 @@ export BLUE_SERVICE=${BLUE_SERVICE}
 export GREEN_SERVICE=${GREEN_SERVICE}
 export count1=${count2}
 docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
+echo ${CI_REGISTRY_USER}
+echo ${CI_REGISTRY_PASSWORD}
+echo ${CI_REGISTRY}
 echo ${BACKEND_VERSION}
 # docker network create -d bridge sausage_network || true
 echo ${REPORTS_MONGODB_URI}
